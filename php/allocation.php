942fca7946bfa7305adbd2ec0dbb923fa79e2fdb
DuckThangg <tranthang181103k@gmail.com>
2023-12-19 23:51:42 +0700

    update allocation

--- php/allocation.php
+++ php/allocation.php
@@ -28,43 +28,48 @@
     <?php
         require 'connect.php';
         mysqli_set_charset($conn, 'UTF8');
-        
+
         if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['id_lop']) && isset($_POST['update_teacher'])) {
             $id_lop = $_POST['id_lop'];
             $update_teacher = $_POST['update_teacher'];
         
-            // Cập nhật trường id_gv_phu_trach trong bảng lop
-            $sql_update_id_gv = "UPDATE lop SET id_gv_phu_trach = '$update_teacher' WHERE id_lop = '$id_lop'";
+            // Kiểm tra xem ID giáo viên tồn tại trong bảng giao_vien hay không
+            $check_teacher_query = "SELECT id_giao_vien FROM giao_vien WHERE id_giao_vien = '$update_teacher'";
+            $check_teacher_result = $conn->query($check_teacher_query);
         
-            if ($conn->query($sql_update_id_gv) === TRUE) {
-                echo "<h1>Cập nhật ID giáo viên phụ trách thành công!</h1>";
+            if ($check_teacher_result->num_rows > 0) {
+                // Nếu ID giáo viên tồn tại, thực hiện cập nhật trong bảng lop
+                $sql_update_id_gv = "UPDATE lop SET id_gv_phu_trach = '$update_teacher' WHERE id_lop = '$id_lop'";
             
-                // Sau khi cập nhật id_gv_phu_trach, cần lấy thông tin gv_phu_trach từ bảng giao_vien và cập nhật lại vào bảng lop
-                $sql_get_teacher_info = "SELECT ten_giao_vien FROM giao_vien WHERE id_giao_vien = '$update_teacher'";
-                $result = $conn->query($sql_get_teacher_info);
-            
-                if ($result->num_rows > 0) {
-                    $row = $result->fetch_assoc();
-                    $teacher_name = $row['ten_giao_vien'];
+                if ($conn->query($sql_update_id_gv) === TRUE) {
+                    // Cập nhật thành công ID giáo viên phụ trách, tiến hành lấy thông tin giáo viên và cập nhật gv_phu_trach
+                    $sql_get_teacher_info = "SELECT ten_giao_vien FROM giao_vien WHERE id_giao_vien = '$update_teacher'";
+                    $result = $conn->query($sql_get_teacher_info);
                 
-                    // Cập nhật trường gv_phu_trach trong bảng lop với thông tin từ bảng giao_vien
-                    $sql_update_gv_phu_trach = "UPDATE lop SET gv_phu_trach = '$teacher_name' WHERE id_lop = '$id_lop'";
-                    if ($conn->query($sql_update_gv_phu_trach) === TRUE) {
-                        echo "<p>Cập nhật giáo viên phụ trách thành công!</p>";
+                    if ($result->num_rows > 0) {
+                        $row = $result->fetch_assoc();
+                        $teacher_name = $row['ten_giao_vien'];
+                        // Cập nhật trường gv_phu_trach trong bảng lop với thông tin từ bảng giao_vien
+                        $sql_update_gv_phu_trach = "UPDATE lop SET gv_phu_trach = '$teacher_name' WHERE id_lop = '$id_lop'";
+                        if ($conn->query($sql_update_gv_phu_trach) === TRUE) {
+                            echo "<p>Cập nhật giáo viên phụ trách thành công!</p>";
+                        } else {
+                            echo "Lỗi khi cập nhật giáo viên phụ trách: " . $conn->error;
+                        }
                     } else {
-                        echo "Lỗi khi cập nhật giáo viên phụ trách: " . $conn->error;
+                        echo "<p>Không tìm thấy thông tin giáo viên!</p>";
                     }
                 } else {
-                    echo "Không tìm thấy thông tin giáo viên!";
+                    echo "Lỗi khi cập nhật ID giáo viên phụ trách: " . $conn->error;
                 }
             } else {
-                echo "Lỗi khi cập nhật ID giáo viên phụ trách: " . $conn->error;
+                echo "<h1>Không tìm thấy id giáo viên!</h1>";
             }
         }
-        
+
         $sql = "SELECT * FROM lop WHERE gv_phu_trach IS NULL OR gv_phu_trach = ''";
         $result = $conn->query($sql);
-        
+
         if ($result->num_rows > 0) {
             echo "<table class='my-table' border='2'>
                     <tr>
@@ -95,9 +100,8 @@
         } else {
             echo "Không có thông tin để hiển thị";
         }
-        
         $conn->close();
-?>
-</div>
+    ?>
+    </div>
 </body>
 </html>
